0472ab523d741266589e676947cc2266d0f69671
Mateusz Boczek <mateusz1_boczek1@interia.pl>
2026-09-18 21:47:57 +0200

    extended hello func with default behaviour

--- hello_test.go
+++ hello_test.go
@@ -3,9 +3,21 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Chris")
-	want := "Hello, Chris"
+	t.Run("saying, hello to people", func(t *testing.T) {
+		got := Hello("Chris")
+		want := "Hello, Chris"
+		assertCorrectMessage(t, got, want)
+	})
 
+	t.Run("say 'Hello' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, world"
+		assertCorrectMessage(t, got, want)
+	})
+}
+
+func assertCorrectMessage(t testing.TB, got string, want string) {
+	t.Helper() //t.Helper() is needed to tell the test suite that this method is a helper. 
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
