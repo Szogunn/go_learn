fa429fce52c58ec9116e54655a5674f3390554c3
Mateusz Boczek <mateusz1_boczek1@interia.pl>
2026-09-18 22:35:31 +0200

    extended Hello to handle greeting in multiple languages

--- hello_test.go
+++ hello_test.go
@@ -4,20 +4,32 @@ import "testing"
 
 func TestHello(t *testing.T) {
 	t.Run("saying, hello to people", func(t *testing.T) {
-		got := Hello("Chris")
+		got := Hello("Chris", "")
 		want := "Hello, Chris"
 		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("say 'Hello' when an empty string is supplied", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, world"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Eloide", "Spanish")
+		want := "Hola, Eloide"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Frank", "French")
+		want := "Bonjour, Frank"
 		assertCorrectMessage(t, got, want)
 	})
 }
 
 func assertCorrectMessage(t testing.TB, got string, want string) {
-	t.Helper() //t.Helper() is needed to tell the test suite that this method is a helper. 
+	t.Helper() //t.Helper() is needed to tell the test suite that this method is a helper.
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
