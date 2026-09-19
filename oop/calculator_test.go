960ac606b38ee26929a3b2a3b4bf18a8620fb563
Mateusz Boczek <mateusz1_boczek1@interia.pl>
2026-09-19 17:27:34 +0200

    struct Triangle implemented with tests

--- oop/calculator_test.go
+++ oop/calculator_test.go
@@ -15,18 +15,23 @@ func TestPerimeter(t *testing.T) {
 
 func TestArea(t *testing.T) {
 
-	areaTests := [] struct {
+	areaTests := []struct {
+		name  string
 		shape Shape
-		want float64
-	} {
-		{Rectangle{12, 6}, 72},
-		{Circle{10}, 314.1592653589793},
+		want  float64
+	}{
+		{name: "Recnagle", shape: Rectangle{12, 6}, want: 72},
+		{name: "Circle", shape: Circle{10}, want: 314.1592653589793},
+		{name: "Triangle", shape: Triangle{12, 6}, want: 36.0},
 	}
 
 	for _, tt := range areaTests {
-		got := tt.shape.Area()
-		if got != tt.want {
-			t.Errorf("got %g want %g", got, tt.want)
-		}
+		// using tt.name from the case to use it as the `t.Run` test name
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
+			if got != tt.want {
+				t.Errorf("%#v got %g want %g",tt.shape, got, tt.want)
+			}
+		})
 	}
 }
