1d146012e11e08925fa3a6420de9a53a9ee56a51
Mateusz Boczek <mateusz1_boczek1@interia.pl>
2026-09-20 15:21:14 +0200

    new SpySleeper struct and deleted previous (redundant) sleep struct

--- mocking/mock_test.go
+++ mocking/mock_test.go
@@ -2,18 +2,19 @@ package mocking
 
 import (
 	"bytes"
+	"reflect"
 	"testing"
 )
 
 func TestCountdow(t *testing.T) {
-	t.Run("basic Countdown", func(t *testing.T) {
+	t.Run("prints 3 to Go!", func(t *testing.T) {
 		buffer := &bytes.Buffer{}
-		spySleeper := &SpySleeper{0}
+		spySleeper := &SpyCountdownOperations{}
 
 		Countdown(buffer, spySleeper)
 
 		got := buffer.String()
-			want := `3
+		want := `3
 2
 1
 Go!`
@@ -21,11 +22,25 @@ Go!`
 		if got != want {
 			t.Errorf("got %s, want %s", got, want)
 		}
+	})
 
-		if spySleeper.Calls != 3 {
-			t.Errorf("not enough calls to sleeper, want 3 but got %d", spySleeper.Calls)
+	t.Run("sleep before every print", func(t *testing.T) {
+		spy := &SpyCountdownOperations{}
+		Countdown(spy, spy)
+
+		want := []string{
+			"write",
+			"sleep",
+			"write",
+			"sleep",
+			"write",
+			"sleep",
+			"write",
 		}
 
+		if !reflect.DeepEqual(want, spy.Calls) {
+			t.Errorf("wanted calls %v got %v", want, spy.Calls)
+		}
 	})
 
 }
